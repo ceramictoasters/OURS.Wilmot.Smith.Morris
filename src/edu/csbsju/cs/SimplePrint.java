6783d47e84b7675f677186d36d3a9bd81895b0d5
CTATE001 <ctate001@vplincsci025.ad.csbsju.edu>
2018-03-12 19:28:35 -0500

    Committing gitig BDLibDr and SimplePr

---
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -0,0 +1,69 @@
+package edu.csbsju.cs;
+
+/**
+ * Ceramictoasters eclipse exercise
+ * @author Wilmot, Connor Smith, and Richard Morris
+ * @version Merch 12th, 2018
+ */
+public class SimplePrint {
+	private String pName;
+	private int pAge;
+	
+	/**
+	 * @param pName
+	 * @param pAge
+	 */
+	public SimplePrint(String pName, int pAge) {
+		super();
+		this.pName = pName;
+		this.pAge = pAge;
+	}
+	
+	/**
+	 * Default
+	 */
+	public SimplePrint() {
+		this.pName = null;
+		this.pAge = -1;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Wilmot\nConnor Smith\nRichard Morris");
+
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("args[ " + i + " ] = " + args[i]);
+		}
+	}
+
+	/**
+	 * @return the pName
+	 */
+	public String getpName() {
+		return pName;
+	}
+
+	/**
+	 * @param pName the pName to set
+	 */
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+	/**
+	 * @return the pAge
+	 */
+	public int getpAge() {
+		return pAge;
+	}
+
+	/**
+	 * @param pAge the pAge to set
+	 */
+	public void setpAge(int pAge) {
+		this.pAge = pAge;
+	}
+}
